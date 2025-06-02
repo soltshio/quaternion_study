5b21babc4f417526a91ea625285f683a50441e9c
soltshio <shanshanshien@gmail.com>
2025-06-02 17:11:35 +0900

    TestSlerp以外をクリア

--- src/libquat/quaternion.cpp
+++ src/libquat/quaternion.cpp
@@ -48,13 +48,24 @@ quaternion quaternion::operator/(float f) const
 vector3 quaternion::operator*(const vector3& v) const
 {
 	// todo: 実装して下さい
-	return vector3();
+	quaternion v_qua(v.x(), v.y(), v.z(), 0);
+
+	quaternion q_conj = this->conjugate();
+
+	quaternion q_ret = *this * v_qua * q_conj;
+
+	return vector3(q_ret.x_, q_ret.y_, q_ret.z_);
 }
 
 quaternion quaternion::operator*(const quaternion& rhs) const
 {
 	// todo: 実装して下さい
-	return quaternion();
+	return quaternion(
+	w_*rhs.x_ + x_*rhs.w_ + y_*rhs.z_ - z_*rhs.y_,//x
+	w_*rhs.y_ - x_*rhs.z_ + y_*rhs.w_ + z_*rhs.x_,//y
+	w_*rhs.z_ + x_*rhs.y_ - y_*rhs.x_ + z_*rhs.w_,//z
+	w_*rhs.w_ - x_*rhs.x_ - y_*rhs.y_ - z_*rhs.z_//w
+	);
 }
 
 quaternion quaternion::operator+(const quaternion& rhs) const
@@ -67,6 +78,10 @@ quaternion quaternion::operator+(const quaternion& rhs) const
 quaternion &quaternion::identity()
 {
 	// todo: 実装して下さい
+	x_ = 0.0f;
+	y_ = 0.0f;
+	z_ = 0.0f;
+	w_ = 1.0f;
 	return *this;
 }
 
@@ -74,6 +89,8 @@ quaternion &quaternion::identity()
 quaternion &quaternion::normalize()
 {
 	// todo: 実装して下さい
+	float length = std::sqrt(this->length_sq());
+	*this = *this / length;
 	return *this;
 }
 
@@ -87,14 +104,18 @@ float quaternion::length_sq() const
 quaternion quaternion::conjugate() const
 {
 	// todo: 実装して下さい
-	return quaternion();
+	return quaternion(-x_, -y_, -z_, w_);
 }
 
 // 逆元を返す
 quaternion quaternion::inverse() const
 {
 	// todo: 実装して下さい
-	return quaternion();
+	float norm = this->length_sq();
+
+	quaternion ret = this->conjugate() / norm;
+
+	return ret;
 }
 
 // 球面線形補間
